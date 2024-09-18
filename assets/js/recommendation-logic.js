b9803ef564be0d8a6bb303603f84f8293ec75af1
“Michael <michaelprattcoding@gmail.comgit config --global core.editor code>
2024-09-17 19:43:26 -0700

    new changes do not add

--- assets/js/recommendation-logic.js
+++ assets/js/recommendation-logic.js
@@ -1,1 +1,8 @@
-
+// Hardcoded weather recommendations
+const recommendations = {
+  freezing: ["Reykjavik", "Juneau", "Svalbard"],
+  cold: ["Toronto", "Moscow", "Berlin"],
+  neutral: ["San Francisco", "Vancouver", "Paris"],
+  warm: ["Maui", "Miami", "Los Angeles"],
+  hot: ["Dubai", "Bangkok", "Cairo"],
+};
