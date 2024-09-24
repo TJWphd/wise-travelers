926bed3fd02c4c91600b206d41505e54db0728cd
“Thomas <tjwhitephd@gmail.com>
2024-09-23 19:14:30 -0700

    fixed cities even more

--- assets/js/recommendation-logic.js
+++ assets/js/recommendation-logic.js
@@ -1,8 +1,8 @@
 //hard coded weather for wise traveler recommendation
 const recommendations = {
   chilly: ["Alaska", "Iceland", "Norway"],
-  cool: ["Toronto, ON, CN", "Saint Petersburg, Russia", "Hokkaido, Japan"],
+  cool: ["Montreal, Quebec", "Saint Petersburg", "Hokkaido, Japan"],
   temperate: ["San Francisco, CA, US", "Hamburg", "Xi'an"],
   tropical: ["Maui, HI, US", "Miami", "Kerala, India"],
-  desert: ["Namibia", "Cairo, Egypt", "Burj Khalifa"],
+  desert: ["Namibia", "Cairo, Egypt", "Rabat"],
 };
